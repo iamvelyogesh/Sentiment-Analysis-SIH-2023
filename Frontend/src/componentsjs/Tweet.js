80cbc4bd3bf9b7a2644dc2a864900a8842278dc6
VEL YOGESH <113898735+iamvelyogesh@users.noreply.github.com>
2023-12-17 00:33:52 +0530

    Update Tweet.js

--- Frontend/src/componentsjs/Tweet.js
+++ Frontend/src/componentsjs/Tweet.js
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { PieChart, Pie, Cell, Tooltip  } from 'recharts';
+import './Tweet.css';
+import Navigationn2 from '../components/navigation2';
+
+import a from '../images/t.jpg';
 // ... (import statements)
 
 const Twet = () => {
   const [inputText, setInputText] = useState('');
   const [inputNumber, setInputNumber] = useState(0);
   const [selectedEmotion, setSelectedEmotion] = useState('all');
-  const [analysisResult, setAnalysisResult] = useState([]); // Initialize as an empty array
+  const [analysisResult, setAnalysisResult] = useState([]); 
+  const [sentimentDistribution, setSentimentDistribution] = useState(null);
+
 
   const handleTextAnalysis = async () => {
     try {
@@ -17,9 +24,79 @@ const Twet = () => {
       });
       const result = response.data.Result;
       setAnalysisResult(result);
+      const positiveCount = result.filter((r) => r.compound_score_sentiment === 'Positive').length;
+      const negativeCount = result.filter((r) => r.compound_score_sentiment === 'Negative').length;
+      const neutralCount = result.filter((r) => r.compound_score_sentiment === 'Neutral').length;
+
+      const total = positiveCount + negativeCount + neutralCount;
+
+      setSentimentDistribution([
+        { name: 'Positive', value: positiveCount, percentage: (positiveCount / total) * 100 },
+        { name: 'Negative', value: negativeCount, percentage: (negativeCount / total) * 100 },
+        { name: 'Neutral', value: neutralCount, percentage: (neutralCount / total) * 100 },
+      ]);
     } catch (error) {
       console.error('Error while making the request:', error);
     }
+  };
+
+  const renderPieChart = () => {
+    if (sentimentDistribution) {
+      const COLORS = ['#36A2EB', '#FF6384', '#FFCE56'];
+
+      return (
+        <div >
+          <h2 className='x1'>Sentiment Distribution</h2>
+          <ul className="list-container1">
+          <li className="list-item1">
+            <p>
+Text analysis employs computational methods to extract meaningful insights and patterns from written content, facilitating tasks such as sentiment analysis and information extraction.</p>
+          </li>
+          <li className="list-item">
+            <p>It analyzes the input and predicts the sentiment or emotion in the content.</p>
+          </li>
+          
+        </ul>
+          <div className='rt'>
+          <PieChart width={500} height={500} className='df' >
+            <Pie
+              data={sentimentDistribution}
+              dataKey="value"
+              nameKey="name"
+              cx="50%"
+              cy="50%"
+              outerRadius={170}
+              fill="#8884d8"
+              label
+            >
+              {sentimentDistribution.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+              ))}
+            </Pie>
+            <Tooltip formatter={(value, name) => [`${value} (${name === 'percentage' ? '%' : 'count'})`, name]} />
+            
+          </PieChart>
+         
+          <div className='legend'>
+            <div className='legend-item'>
+              <span className='legend-color negative ' style={{ backgroundColor: COLORS[1], width: '30px', height: '30px', borderRadius: '50%', margin: '5px', fontSize: '200px',alignItems: 'center', justifyContent: 'center' }}></span>
+              <p className='x'>Negative</p>
+            </div>
+            <div className='legend-item'>
+              <span className='legend-color neutral' style={{ backgroundColor: COLORS[2], width: '30px', height: '30px', borderRadius: '50%', margin: '5px' }}></span>
+              Neutral
+            </div>
+            <div className='legend-item'>
+              <span className='legend-color positive' style={{ backgroundColor: COLORS[0], width: '30px', height: '30px', borderRadius: '50%', margin: '5px' }}></span>
+              Positive
+            </div>
+          </div>
+
+          </div>
+        </div>
+      );
+    }
+    return null;
   };
 
   const renderTable = () => {
@@ -39,6 +116,9 @@ const Twet = () => {
 
   const renderAllTable = () => {
     return (
+      <div className='xc'>
+        <h2 className='x'>Sentiment Distribution Table</h2>
+      
       <table>
           <thead>
             <tr>
@@ -69,6 +149,7 @@ const Twet = () => {
             ))}
           </tbody>
         </table>
+        </div>
     );
   };
 
@@ -185,17 +266,20 @@ const Twet = () => {
 
   return (
     <div>
-      <div className='container11'>
-        <h1 className='sdaasd'>Detecting and Monitoring Hate Content</h1>
+      <Navigationn2/>
+      <div className='container113'>
+        {/* <h1 className='sdaasd'>Tweet Analysis</h1> */}
         <div style={{ display: 'flex' }}>
           <div style={{ flex: 1 }}>
-            <div>
+            <div >
               <h2 className='x'>About</h2>
-              {/* ... (existing about section) */}
+              <div className='ws'>
+                   <img src={a}></img>
+              </div>
             </div>
           </div>
           <div style={{ flex: 1 }}>
-            <h2 className='x'>Text Analysis</h2>
+            <h2 className='x'>Tweet Analysis</h2>
             <textarea
               placeholder="Enter text for analysis..."
               value={inputText}
@@ -221,7 +305,11 @@ const Twet = () => {
             <button onClick={handleTextAnalysis}>Analyse Text</button>
           </div>
         </div>
+        
         {renderTable()}
+        
+        {renderPieChart()}
+        
       </div>
     </div>
   );
